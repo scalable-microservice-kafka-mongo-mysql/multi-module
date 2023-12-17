821ff32843fa3aa7e75880d7c940a3fbd5c6bf8f
Ashutosh Pathak <ashu.p89@gmail.com>
2023-12-17 11:33:10 +0530

    list of object passed to get inventory controller as query param

--- inventory-service/src/main/java/com/ashutoshpathak/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/ashutoshpathak/inventoryservice/controller/InventoryController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/inventory")
 public class InventoryController {
@@ -12,10 +14,18 @@ public class InventoryController {
     @Autowired
     InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping("/check")
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
-        return inventoryService.isInStock(skuCode);
+    public boolean isInStock(@RequestParam("skuCode") List<String> skuCodes,
+                             @RequestParam("quantity") List<Integer> quantities) {
+        // Assuming equal size for skuCodes and quantities
+        for (int i = 0; i < skuCodes.size(); i++) {
+            String skuCode = skuCodes.get(i);
+            Integer quantity = quantities.get(i);
+            if (!inventoryService.isInStock(skuCode, quantity)) {
+                return false;
+            }
+        }
+        return true;
     }
-
 }
--- order-service/src/main/java/com/ashutoshpathak/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ashutoshpathak/orderservice/service/OrderService.java
@@ -8,7 +8,9 @@ import com.ashutoshpathak.orderservice.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -36,20 +38,39 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        //orderRepository.save(order);
 
-//        //Call inventory service and place order if product is in stock
+        //Call inventory service and place order if product is in stock
 //        Boolean inventoryExists = webClient.get()
 //                .uri("http://localhost:8082/api/inventory")
 //                .retrieve()
 //                .bodyToMono(Boolean.class)
 //                .block();
 
-//        if(Boolean.TRUE.equals(inventoryExists)) {
-//            orderRepository.save(order);
-//        } else{
-//            throw new IllegalArgumentException("insufficient inventory");
-//        }
+        //List<CustomObject> customObjects = Arrays.asList(/* your list of CustomObjects */);
+
+        List<OrderLineItemsDTO> orderLineItemsList = orderRequest.getOrderLineItemsRequestList();
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString("http://localhost:8082/api/inventory");
+
+        for (OrderLineItemsDTO orderLineItem : orderLineItemsList) {
+            uriBuilder.queryParam("skuCode", orderLineItem.getSkuCode());
+            uriBuilder.queryParam("quantity", orderLineItem.getQuantity());
+        }
+
+        String uriWithParams = uriBuilder.toUriString();
+
+        Boolean inventoryExists = webClient.get()
+                .uri(uriWithParams)
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+
+        if(Boolean.TRUE.equals(inventoryExists)) {
+            orderRepository.save(order);
+        } else{
+            throw new IllegalArgumentException("insufficient inventory");
+        }
     }
 
     private OrderLineItems mapToOrderLineItems(OrderLineItemsDTO orderLineItemsDTO) {
