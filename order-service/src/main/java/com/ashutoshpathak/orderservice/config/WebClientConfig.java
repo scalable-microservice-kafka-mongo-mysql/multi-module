249a6482fa17c58e105c9a2142b8bbefd377b5be
Ashutosh Pathak <ashu.p89@gmail.com>
2023-12-17 11:46:05 +0530

    code polish

--- order-service/src/main/java/com/ashutoshpathak/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/ashutoshpathak/orderservice/config/WebClientConfig.java
@@ -7,6 +7,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
     @Bean
+    //Spring beans have same name as the methodName instantiating them.
+    //So if i @Autowire somewhere .... , that object's name i'll have to make as webClient itself!
     public WebClient webClient(){
         return WebClient.builder().build();
     }
